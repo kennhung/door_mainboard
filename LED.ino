e58bde25a568731fe54faff17409795d64cec77d
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2016-08-11 23:19:01 +0800

    first commit

---
+++ LED.ino
@@ -0,0 +1,96 @@
+const int redLed = 26;
+const int greenLed = 30;
+const int blueLed = 27;
+
+//#define COMMON_ANODE
+
+#ifdef COMMON_ANODE
+#define LED_ON LOW
+#define LED_OFF HIGH
+#else
+#define LED_ON HIGH
+#define LED_OFF LOW
+#endif
+
+
+void ledsetup(){
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(blueLed, OUTPUT);
+  blueSolid();
+}
+
+
+//////////////////////////////////////// Normal Mode Led  ///////////////////////////////////
+void blueSolid () {
+  digitalWrite(blueLed, LED_ON);   // Blue LED ON and ready to read card
+  digitalWrite(redLed, LED_OFF);  // Make sure Red LED is off
+  digitalWrite(greenLed, LED_OFF);  // Make sure Green LED is off
+}
+
+void greenSolid () {
+  digitalWrite(blueLed, LED_OFF);   // Turn off blue LED
+  digitalWrite(redLed, LED_OFF);  // Turn off red LED
+  digitalWrite(greenLed, LED_ON);   // Turn on green LED
+  delay(2000);
+  
+}
+
+void redSolid () {
+  digitalWrite(blueLed, LED_OFF);   // Turn off blue LED
+  digitalWrite(redLed, LED_ON);  // Turn on red LED
+  digitalWrite(greenLed, LED_OFF);   // Turn off green LED
+  delay(2000);
+}
+
+void redBlink() {
+  digitalWrite(blueLed, LED_OFF);   // Make sure blue LED is off
+  digitalWrite(redLed, LED_OFF);  // Make sure red LED is off
+  digitalWrite(greenLed, LED_OFF);  // Make sure green LED is off
+  delay(200);
+  digitalWrite(redLed, LED_ON);   // Make sure red LED is on
+  delay(200);
+  digitalWrite(redLed, LED_OFF);  // Make sure red LED is off
+  delay(200);
+  digitalWrite(redLed, LED_ON);   // Make sure red LED is on
+  delay(200);
+  digitalWrite(redLed, LED_OFF);  // Make sure red LED is off
+  delay(200);
+  digitalWrite(redLed, LED_ON);   // Make sure red LED is on
+  delay(200);
+}
+
+void blueBlink() {
+  digitalWrite(blueLed, LED_OFF);   // Make sure blue LED is off
+  digitalWrite(redLed, LED_OFF);  // Make sure red LED is off
+  digitalWrite(greenLed, LED_OFF);  // Make sure green LED is off
+  delay(200);
+  digitalWrite(blueLed, LED_ON);  // Make sure blue LED is on
+  delay(200);
+  digitalWrite(blueLed, LED_OFF);   // Make sure blue LED is off
+  delay(200);
+  digitalWrite(blueLed, LED_ON);  // Make sure blue LED is on
+  delay(200);
+  digitalWrite(blueLed, LED_OFF);   // Make sure blue LED is off
+  delay(200);
+  digitalWrite(blueLed, LED_ON);  // Make sure blue LED is on
+  delay(200);
+}
+
+void greenBlink() {
+  digitalWrite(blueLed, LED_OFF);   // Make sure blue LED is off
+  digitalWrite(redLed, LED_OFF);  // Make sure red LED is off
+  digitalWrite(greenLed, LED_OFF);  // Make sure green LED is on
+  delay(200);
+  digitalWrite(greenLed, LED_ON);   // Make sure green LED is on
+  delay(200);
+  digitalWrite(greenLed, LED_OFF);  // Make sure green LED is off
+  delay(200);
+  digitalWrite(greenLed, LED_ON);   // Make sure green LED is on
+  delay(200);
+  digitalWrite(greenLed, LED_OFF);  // Make sure green LED is off
+  delay(200);
+  digitalWrite(greenLed, LED_ON);   // Make sure green LED is on
+  delay(200);
+}
+
---
+++ door_mainboard.ino
@@ -0,0 +1,149 @@
+
+#include <SPI.h>        // RC522 Module uses SPI protocol
+#include <SD.h>         // We are going to read and write PICC's UIDs from/to SD
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+File myFile;
+byte readCard[4];    // Stores scanned ID read from RFID Module
+byte masterCard[4];   // Stores master card's ID
+char dir[] = "PICCS/";
+char masdir[] = "master.txt";
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // 設定 LCD I2C 位址
+
+File masterfile;
+int successRead;
+void setup() {
+  Serial.begin(9600);
+  successRead = 0;
+  lcd.begin(16, 2);
+  // 閃爍三次
+  for(int i = 0; i < 3; i++) {
+    lcd.backlight(); // 開啟背光
+    delay(250);
+    lcd.noBacklight(); // 關閉背光
+    delay(250);
+  }
+  ledsetup();
+  Wire.begin(8);
+  Wire.onReceive(receiveEvent);
+  Serial.print("Initializing SD card...");
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  if (!SD.begin(53)) {
+    Serial.println("initialization failed!");
+    lcd.print("error");
+    redSolid();
+    return;
+  }
+  Serial.println("initialization done.");
+  
+  readMaster();
+}
+
+void loop() {
+  int stat = checkID();
+  if(stat==1){
+    redSolid();
+  }
+  else if(stat==0){
+    greenSolid();
+  }
+
+  if(successRead){
+    Serial.println("read");
+    successRead=0;
+  }
+  else{
+    for(int i =0; i<4;i++){
+      readCard[i] = 0;
+    }
+    blueSolid();
+  }
+  
+}
+
+void readMaster(){
+  if (SD.exists(masdir)) {
+    Serial.println("Master card exists.");
+    Serial.print(F("Master Card's UID: "));      // Since we have it print to serial
+    File masterfile = SD.open(masdir);  // Open file
+    for (int i = 0; i < 4; i++) {             // Loop 4 times to get 4 bytes
+      readCard[i] = masterfile.read();
+      Serial.print(readCard[i], HEX);         // Actual serial printing of each byte
+      masterCard[i] = readCard[i];            // Prepare bytes for future comparing
+    }
+    Serial.println("");
+    masterfile.close();
+  } else {
+    Serial.println("Master card doesn't exist.");
+    Serial.println(F("Scan A PICC to Define as Master Card"));
+    do {
+      blueBlink(); // Visualize Master Card need to be defined
+    }
+    while (!successRead); //the program will not go further while you not get a successful read
+    delay(100);
+    Serial.println("Creating Master card");
+    masterfile = SD.open(masdir,FILE_WRITE);
+    if(masterfile){
+      Serial.println("Writing to master.txt...");
+      while(!successRead);
+      masterfile.write(readCard, 4); 
+      for (int i = 0; i < 4; i++) {             // Loop 4 times to get 4 bytes
+        readCard[i] = masterfile.read();
+
+        masterCard[i] = readCard[i];            // Prepare bytes for future comparing
+      }
+      // close the file:
+      masterfile.close();
+      Serial.println(F("Master Card successfuly defined"));
+      for (int i = 0; i < 4; i++) {  //
+       Serial.print(masterCard[i], HEX);
+     }
+     greenBlink();
+    }
+    else{
+      Serial.println("creating master file fail!!!");
+      redBlink();
+    }
+  }
+}
+
+void readUIDCard(){
+     Serial.println(F("Scanned PICC's UID: "));
+     for (int i = 0; i < 4; i++) {  //
+       Serial.print(readCard[i],HEX);
+     }
+     Serial.println();
+  }
+
+void receiveEvent(int howMany){
+  int i = 0;
+  while(Wire.available()){
+    byte a = Wire.read();
+    readCard[i] = a;
+    i++;
+  }
+  successRead = 1;
+  readUIDCard();
+  checkID();
+}
+
+int checkID(){
+  if(successRead){
+    for(int i = 0; i<4; i++){
+      if(readCard[i] == masterCard[i]){
+        
+      }
+      else{
+        return 1;
+      }
+    }
+    return 0;
+  }
+  else{
+    return 2;
+  }
+}
+
